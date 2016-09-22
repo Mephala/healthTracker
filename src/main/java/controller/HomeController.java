3e547a508cdd39309f6e0bc50bca0d563bf20b24
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2016-09-22 20:06:39 +0300

    Initial commit

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+
+    @RequestMapping(value = "/")
+    public String home() {
+        System.out.println("Home-Controller");
+        return "home";
+    }
+}
